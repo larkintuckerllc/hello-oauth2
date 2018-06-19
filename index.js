38df880f3a17508c1bc19f7897fce7d96bccd9a2
John Tucker <john.tucker@sharpspring.com>
2018-06-18 21:00:26 -0400

    Finish

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Configstore = require('configstore');
 const express = require('express');
 const { google } = require('googleapis');
 const pkg = require('./package.json');
+const fs = require('fs');
 
 const client_id = config.get('client_id');
 const client_secret = config.get('client_secret');
@@ -16,6 +17,8 @@ const oauth2Client = new google.auth.OAuth2(
 const scopes = [
     'https://mail.google.com/'
 ];
+const success = fs.readFileSync('./success.html', 'utf8');
+const error = fs.readFileSync('./error.html', 'utf8');
 app.use(express.static('public'));
 app.get('/login', (req, res) => {
     const user = req.query.user;
@@ -29,7 +32,7 @@ app.get('/login', (req, res) => {
 app.get('/oauth2callback', async (req, res) => {
     const code = req.query.code;
     if (code === undefined) {
-        res.send('UNAUTHORIZED');
+        res.send(error);
         return;
     }
     const user = req.query.state; // TODO: DECRYPT
@@ -41,10 +44,45 @@ app.get('/oauth2callback', async (req, res) => {
         conf.set('access_token', access_token);
         conf.set('refresh_token', refresh_token);
         conf.set('expiry_date', expiry_date_string);
-        res.send('SUCCESS');
+        res.send(success);
     } catch(error) {
-        res.send(`ERROR: ${error}`);
+        res.send(error);
     }
+});
+app.get('/send', async (req, res) => {
+    const access_token = conf.get('access_token');
+    console.log(access_token);
+    oauth2Client.credentials = {
+        access_token,
+    };
+    const gmailClass = google.gmail({
+        version: 'v1',
+        auth: oauth2Client,
+    });
+    var email_lines = [];
+
+    email_lines.push('From: "test" <john.sckmkny.com@gmail.com>');
+    email_lines.push('To: john.tucker@sharpspring.com');
+    email_lines.push('Content-type: text/html;charset=iso-8859-1');
+    email_lines.push('MIME-Version: 1.0');
+    email_lines.push('Subject: this would be the subject');
+    email_lines.push('');
+    email_lines.push('And this would be the content.<br/>');
+    email_lines.push('The body is in HTML so <b>we could even use bold</b>');
+    var email = email_lines.join('\r\n').trim();
+    var base64EncodedEmail = new Buffer(email).toString('base64');
+    base64EncodedEmail = base64EncodedEmail.replace(/\+/g, '-').replace(/\//g, '_');
+    gmailClass.users.messages.send({
+      userId: 'me',
+      resource: {
+        raw: base64EncodedEmail
+      }
+    }, (err, response) => {
+        console.log(err);
+        console.log(response);
+        res.send('done');
+    });
 });
 // TODO: WORKER PROCESS TO REFRESH SOON TO EXPIRE TOKENS
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
